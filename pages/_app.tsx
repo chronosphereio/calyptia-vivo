101e1a64109db577efd1b2bca85feae68d917f1c
Nicolás Parada <nicolas@calyptia.com>
2022-10-25 15:43:34 -0300

    material design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,32 @@
-import '../styles/globals.css'
-import '../node_modules/xterm/css/xterm.css'
+import CssBaseline from "@mui/material/CssBaseline"
+import createTheme from "@mui/material/styles/createTheme"
+import ThemeProvider from "@mui/material/styles/ThemeProvider"
 import type { AppProps } from 'next/app'
+import Head from "next/head"
+import 'xterm/css/xterm.css'
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    background: {
+      default: "#f7f8fb"
+    }
+  },
+  typography: {
+    fontFamily: "Rubik, sans-serif",
+  }
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Head>
+        <title>Vivo</title>
+      </Head>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
--- server/index.ts
+++ server/index.ts
@@ -1,28 +1,28 @@
+require('dotenv').config()
+
+import { default as bodyParser, default as express } from 'express'
 import http from 'http'
-
-import express from 'express';
-import bodyParser from 'express';
 /* import session from 'express-session'; */
+import { program } from 'commander'
 import next from 'next'
-import { WebSocketServer } from 'ws';
-import { program } from 'commander';
-
+import { WebSocketServer } from 'ws'
 import flbManager from './flb_manager'
 
-program
-.option('--single-user');
 
-program.parse();
-const opts = program.opts();
+program
+  .option('--single-user')
+
+program.parse()
+const opts = program.opts()
 
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = 'localhost'
-const port = 5489;
+const port = 5489
 const nextApp = next({ dev, hostname, port })
 const nextHandle = nextApp.getRequestHandler()
 
 const app = express()
-.use(bodyParser.json({limit: 1024 * 1024 * 10}))
+  .use(bodyParser.json({ limit: 1024 * 1024 * 10 }))
 
 let healthy = false
 
@@ -40,82 +40,82 @@ app.get('/healthz', (_req, res) => {
       timestamp: Date.now()
     })
   }
-});
+})
 
 let websocketConnections = 0
 
 app.get('/stats', (_req, res) => {
   res.status(200).json({ websocketConnections, fluentBitInstances: manager.count() })
-});
+})
 
 app.get('*', (req, res) => {
   return nextHandle(req, res)
-});
+})
 
 app.post('/flb/:token', async (req, res) => {
-  const { token } = req.params;
+  const { token } = req.params
   try {
-    manager.write(token, req.body);
+    manager.write(token, req.body)
     res.sendStatus(200).end()
   } catch (err) {
-    res.sendStatus(404).end();
+    res.sendStatus(404).end()
   }
-});
+})
 
-const wss = new WebSocketServer({ clientTracking: false, noServer: true });
+const wss = new WebSocketServer({ clientTracking: false, noServer: true })
 const manager = flbManager()
 
-const userId = 'stubUser';
+const userId = 'stubUser'
 
 if (opts.singleUser) {
   const consoleToken = manager.connect(userId, {
     send(json: string) {
-      process.stderr.write("fluent-bit:" + json + "\n", "utf-8");
+      process.stderr.write("fluent-bit:" + json + "\n", "utf-8")
     }
   }, { datasource: 'http' })
 
   app.post('/console', async (req, res) => {
     try {
-      manager.write(consoleToken, req.body);
+      manager.write(consoleToken, req.body)
       res.sendStatus(200).end()
     } catch (err) {
-      res.sendStatus(404).end();
+      res.sendStatus(404).end()
     }
-  });
+  })
 }
 
 wss.on('connection', (ws) => {
-  websocketConnections++;
+  websocketConnections++
 
   ws.once('message', (msg) => {
     try {
-      const opts = JSON.parse(msg.toString());
+      const opts = JSON.parse(msg.toString())
       const token = manager.connect(userId, ws, opts)
-      ws.send(JSON.stringify({ token }));
+      ws.send(JSON.stringify({ token }))
 
       ws.on('close', function () {
-        websocketConnections--;
-        manager.disconnect(userId, ws);
-      });
+        websocketConnections--
+        manager.disconnect(userId, ws)
+      })
     } catch (err) {
-      ws.close();
+      ws.close()
     }
   })
 })
 
-const server = http.createServer(app);
+const server = http.createServer(app)
 server.on('upgrade', function (request: any, socket, head) {
   if (request.url !== '/flb') {
     return
   }
   wss.handleUpgrade(request, socket, head, function (ws) {
-    wss.emit('connection', ws, request);
-  });
+    wss.emit('connection', ws, request)
+  })
 })
 
 async function start() {
   await nextApp.prepare()
-  server.listen(port, (err?: Error) =>{
+  server.listen(port, (err?: Error) => {
     if (err) throw err
     console.log(`> Ready on http://${hostname}:${port}`)
     healthy = true
