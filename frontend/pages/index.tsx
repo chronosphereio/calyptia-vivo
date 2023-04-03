d96ee2cb6f4b381851def6594fe6c89cd0b859a6
Thiago Padilha <thiago@calyptia.com>
2023-04-03 15:08:59 -0300

    Migrate old next.js/typescript UI to vivo-next

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,63 @@
+import TabPanel from "@mui/lab/TabPanel"
+import TabContext from "@mui/lab/TabContext"
+import TabList from "@mui/lab/TabList"
+import Tab from "@mui/material/Tab"
+import type { NextPage } from 'next'
+import Box from "@mui/material/Box"
+import Container from "@mui/material/Container"
+import Card from "@mui/material/Card"
+import CardContent from "@mui/material/CardContent"
+import { useEffect, useState } from 'react'
+import FluentBitData from '../components/FluentBitData'
+import useFluentBitStream, { StreamKind } from '../hooks/useFluentBitStream';
+
+const Home: NextPage = () => {
+
+  return (
+    <Box my={4}>
+      <Container>
+        <Card elevation={0} sx={{ border: "1px solid rgba(63, 81, 181, 0.14)" }}>
+          <CardContent sx={{ bgcolor: "white" }}>
+            <Box>
+              <Box my={2}>
+                <Data />
+              </Box>
+            </Box>
+          </CardContent>
+        </Card>
+      </Container>
+    </Box>
+  )
+}
+
+export default Home
+
+function Data() {
+  const { records, kind, setKind } = useFluentBitStream({
+    vivoExporterUrl: 'http://127.0.0.1:2025',
+    limit: 100,
+    pollInterval: 200
+  });
+
+  return (
+    <TabContext value={kind}>
+      <Box display="grid" gridTemplateColumns="auto 1fr">
+        <Box sx={{ borderRight: 1, borderColor: 'divider' }}>
+          <TabList orientation="vertical" onChange={(_, newVal) => {
+            setKind(newVal)
+          }}>
+            <Tab label="Logs" value="logs" sx={{ whiteSpace: "nowrap" }} />
+            <Tab label="Metrics" value="metrics" sx={{ whiteSpace: "nowrap" }} />
+            <Tab label="Traces" value="traces" sx={{ whiteSpace: "nowrap" }} />
+          </TabList>
+        </Box>
+        <TabPanel value="logs" sx={{ py: 0 }}>
+          <Box>
+            <FluentBitData records={records} />
+          </Box>
+        </TabPanel>
+      </Box>
+    </TabContext >
+  )
+}
+
