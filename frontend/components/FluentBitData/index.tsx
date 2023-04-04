f765dcb90daf3e8caa7d68adfa2f711f0deb0d96
Thiago Padilha <thiago@calyptia.com>
2023-04-04 13:35:06 -0300

    Attempt to fix metrics + metadata

--- frontend/components/FluentBitData/index.tsx
+++ frontend/components/FluentBitData/index.tsx
@@ -11,10 +11,10 @@ import { useState } from 'react'
 
 
 export interface FluentBitDataProps {
-  records: unknown[]
+  records: { id: number, record: unknown}[]
 }
 
-export default function FluentBitData({ records }: FluentBitDataProps) {
+export function FluentBitLogs({ records }: FluentBitDataProps) {
   const [foldMap, setFoldMap] = useState<Record<string, boolean>>({})
 
 
@@ -26,10 +26,11 @@ export default function FluentBitData({ records }: FluentBitDataProps) {
     <Box p={2} bgcolor="#FAFAFA" border="1px solid rgba(63, 81, 181, 0.08)" borderRadius={1} maxHeight="60vh" sx={{ overflowY: "auto" }}>
       <List sx={{ display: "flex", flexDirection: "column-reverse" }}>
         {records.map(r => {
-          const recordId = (r as any)[0][0]; // timestamp
-          const timestamp = Math.floor(recordId / 1e6)
+          const recordId = r.id.toString();
+          const timestamp = Math.floor((r.record as any)[0][0] / 1e6)
           const fold = Object.entries(foldMap).some(([k, v]) => k === recordId && v)
-          const record = (r as any)[1];
+          const record = (r.record as any)[1];
+          const meta = (r.record as any)[0][1];
           return (
             <ListItem key={recordId}>
               <Box borderLeft="3px solid #7B61FF" borderRadius="3px" bgcolor={fold ? "#F1F0F9" : "white"} width="100%" px={2} py={0}>
@@ -47,6 +48,47 @@ export default function FluentBitData({ records }: FluentBitDataProps) {
                     </Stack>
                   </Box>
                   <code style={{ whiteSpace: "pre" }}>{JSON.stringify({ ...record, date: undefined }, null, fold ? 2 : undefined)}</code>
+                  <code style={{ whiteSpace: "pre" }}>{JSON.stringify({ ...meta, date: undefined }, null, fold ? 2 : undefined)}</code>
+                </Stack>
+              </Box>
+            </ListItem>
+          )
+        })}
+      </List>
+    </Box>
+  )
+}
+
+export function FluentBitMetricTraces({ records }: FluentBitDataProps) {
+  const [foldMap, setFoldMap] = useState<Record<string, boolean>>({})
+
+
+  const onFold = (id: string) => {
+    setFoldMap(m => ({ ...m, [id]: !m[id] }))
+  }
+
+  return (
+    <Box p={2} bgcolor="#FAFAFA" border="1px solid rgba(63, 81, 181, 0.08)" borderRadius={1} maxHeight="60vh" sx={{ overflowY: "auto" }}>
+      <List sx={{ display: "flex", flexDirection: "column-reverse" }}>
+        {records.map((r) => {
+          const recordId = r.id.toString();
+          const fold = Object.entries(foldMap).some(([k, v]) => k === recordId.toString() && v)
+          return (
+            <ListItem key={recordId}>
+              <Box borderLeft="3px solid #7B61FF" borderRadius="3px" bgcolor={fold ? "#F1F0F9" : "white"} width="100%" px={2} py={0}>
+                <Stack direction="row" alignItems="center" gap={1}>
+                  <Box>
+                    <Stack direction="row" gap={1} alignItems="center">
+                      <IconButton onClick={() => onFold(recordId.toString())}>
+                        {fold ? (
+                          <UnfoldMore sx={{ color: "#7B61FF" }} />
+                        ) : (
+                          <UnfoldLess sx={{ color: "#7B61FF" }} />
+                        )}
+                      </IconButton>
+                    </Stack>
+                  </Box>
+                  <code style={{ whiteSpace: "pre" }}>{JSON.stringify(r, null, fold ? 2 : undefined)}</code>
                 </Stack>
               </Box>
             </ListItem>
