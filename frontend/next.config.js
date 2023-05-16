682a187958e086c86ea0848dc398ff45243e1e4b
Daniel Fredriksen <daniel@calyptia.com>
2023-05-16 10:47:49 -1000

    Implement Next.js Analyzer

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,4 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+const withBundleAnalyzer = require('@next/bundle-analyzer')( {
+  enabled: process.env.ANALYZE == 'true'
+})
+
+module.exports = withBundleAnalyzer({
+  ...nextConfig
+})
